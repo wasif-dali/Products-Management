61bbed9911c7b3f4c87a869a57727a005ed8ad5b
Manas kumar Jena <you@example.com>
2022-10-11 20:38:35 +0530

    s3

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,7 +1,40 @@
 const userModel = require("../Model/userModel")
 const bcrypt = require('bcrypt');
 const validation = require("../validation/validate")
-// const saltRounds = 10;
+
+
+aws.config.update({
+    accessKeyId: "AKIAY3L35MCRZNIRGT6N",
+    secretAccessKey: "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",
+    region: "ap-south-1"
+})
+
+let uploadFile = async (file) => {
+    return new Promise(function (resolve, reject) {
+
+        let s3 = new aws.S3({ apiVersion: '2006-03-01' });
+
+        var uploadParams = {
+            ACL: "public-read",
+            Bucket: "classroom-training-bucket",  //HERE
+            Key: "abc/" + file.originalname, //HERE 
+            Body: file.buffer
+        }
+
+
+        s3.upload(uploadParams, function (err, data) {
+            if (err) {
+                return reject({ "error": err })
+            }
+            console.log(data)
+            console.log("file uploaded succesfully")
+            return resolve(data.Location)
+        })
+
+
+
+    })
+}
 
 const createUser = async function (req, res) {
     try {
@@ -35,8 +68,8 @@ const createUser = async function (req, res) {
         if (!validation.isvalidpassword(password)) return res.status(400).send({ status: false, msg: "Minimum eight characters, at least one uppercase letter, one lowercase letter and one number: " })
 
         if (!validation.isValidElem(address)) return res.status(400).send({ status: false, msg: "address is required" })
-        
-        
+
+
         // let requiredFields = ["street", "city","pincode"];
         // for (field of requiredFields) {
         //     if (!(req.body.address.shipping.hasOwnProperty(field))) {
@@ -45,11 +78,12 @@ const createUser = async function (req, res) {
         //         .send({ status: false, msg: `this key is not present==>${field}` });
         //     }
         // }
-        
+
 
         const salting = await bcrypt.genSalt(10)
         const newpassword = await bcrypt.hash(password, salting)
         console.log(newpassword)
+
 
 
 
@@ -74,11 +108,20 @@ const createUser = async function (req, res) {
             }
         }
         console.log(document)
+        let files = req.files
+        if (files && files.length > 0) {
+            //upload to s3 and get the uploaded link
+            // res.send the link back to frontend/postman
+            let uploadedFileURL = await uploadFile(files[0])
+            document.profileImage = uploadedFileURL
 
 
-        let saveData = await userModel.create(document)
-        console.log(document)
-
+            let saveData = await userModel.create(document)
+            console.log(document)
+        }
+        else {
+            return res.status(400).send({ status: false, message: "No file found" })
+        }
         return res.status(201).send({
             "status": true,
             "message": "User created successfully",
@@ -92,48 +135,48 @@ const createUser = async function (req, res) {
         })
     }
 }
-const userLogin= async (req,res)=>{
+const userLogin = async (req, res) => {
 
-    try{
-        const loginData=req.body
-        const {email,password}=loginData
-        if(!validation.isValidreqBody(loginData)){
-            return res.status(400).send({status:false,message:"Invalid request,please Enter EmailId and password"})
+    try {
+        const loginData = req.body
+        const { email, password } = loginData
+        if (!validation.isValidreqBody(loginData)) {
+            return res.status(400).send({ status: false, message: "Invalid request,please Enter EmailId and password" })
         }
         //-----------------------------email validation--------------------------------------------
-        if(!email) return res.status(400).send({status:false,message:"please Enter Email"})
-        if(!validation.isValidEmail(email)) return res.status(400).send({satus:false,message:"Please enter a valid email"})
-        if(!validation.isValidElem(email)) return res.status(400).send({status:false,message:"email Id is required"})
+        if (!email) return res.status(400).send({ status: false, message: "please Enter Email" })
+        if (!validation.isValidEmail(email)) return res.status(400).send({ satus: false, message: "Please enter a valid email" })
+        if (!validation.isValidElem(email)) return res.status(400).send({ status: false, message: "email Id is required" })
 
 
         //------------------------------password validation------------------------------------------
 
 
 
-        if(!password) return res.status(400).send({status:false,message:"Please Enter Password"})
-        if(!validation.isValidElem) return res.status(400).send({status:false,message:"password is required"})
-        
-  
-
- 
-        const user=await userModel.findOne({email:email})
-        if(!user) return res.status(401).send({status:false,message:"Invalid Credential"})
-        let MatchUser= await bcrypt.compare(password,user.password)
-        if(!MatchUser) return res.status(401).send({status:false,message:"password does not match"})
+        if (!password) return res.status(400).send({ status: false, message: "Please Enter Password" })
+        if (!validation.isValidElem) return res.status(400).send({ status: false, message: "password is required" })
 
 
-        let token =jwt.sign({userId:user._id.toString(),iat:Math.floor(Date.now()/1000)},
-        "Project5-ProductManagement",
-        { expiresIn:'24h'});
-        res.setHeader("Authorization",token)
-        res.status(200).send({status:true,message:"User Login Succesful",data:{userId:user._id,token:token}})
+
+
+        const user = await userModel.findOne({ email: email })
+        if (!user) return res.status(401).send({ status: false, message: "Invalid Credential" })
+        let MatchUser = await bcrypt.compare(password, user.password)
+        if (!MatchUser) return res.status(401).send({ status: false, message: "password does not match" })
+
+
+        let token = jwt.sign({ userId: user._id.toString(), iat: Math.floor(Date.now() / 1000) },
+            "Project5-ProductManagement",
+            { expiresIn: '24h' });
+        res.setHeader("Authorization", token)
+        res.status(200).send({ status: true, message: "User Login Succesful", data: { userId: user._id, token: token } })
     }
-catch(error){
-    res.status(500).send({status:false,message:error.message})
-}
+    catch (error) {
+        res.status(500).send({ status: false, message: error.message })
+    }
 }
 
-module.exports={
-    createUser,userLogin
+module.exports = {
+    createUser, userLogin
 
 }
