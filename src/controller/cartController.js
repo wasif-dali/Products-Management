a51cb8a4066692f84edd4bd2f89b32fbd4ccfbbf
sabiha <you@example.com>
2022-10-19 12:28:15 +0530

    update cart

--- src/controller/cartController.js
+++ src/controller/cartController.js
@@ -140,4 +140,145 @@ const deleteCart = async function (req, res) {
         return res.status(500).send({ status: false, message: err.message });
     }
 };
-module.exports = { createCart, getCart, deleteCart }
+// update
+
+const updateCart =async (req,res) =>{
+  try {
+      let userId = req.params.userId;
+      let data = req.body;
+      let { productId, cartId, removeProduct } = data;
+  
+      if (Object.keys(data).length == 0)
+        return res
+          .status(400)
+          .send({ status: false, message: "please enter data to update" });
+  
+      if (!validation.isValidObjectId(userId))
+        return res
+          .status(400)
+          .send({ status: false, message: "please enter valid user id" });
+      let user = await userModel.findOne({ _id: userId });
+      if (!user)
+        return res
+          .status(404)
+          .send({ status: false, message: "no such user found" });
+  
+      //Authorisation
+      let tokenUserId = req.userId;
+      if (userId != tokenUserId) {
+        return res
+          .status(403)
+          .send({ status: false, message: "UnAuthorized Access!!" });
+      }
+  
+      if (!validation.isValidObjectId(productId))
+        return res
+          .status(400)
+          .send({ status: false, message: "please enter valid product id" });
+      let findProduct = await productModel.findOne({
+        _id: productId,
+        isDeleted: false,
+      });
+      if (!findProduct)
+        return res
+          .status(404)
+          .send({ status: false, message: "product not found" });
+  
+      if (!validation.isValidObjectId(cartId))
+        return res
+          .status(400)
+          .send({ status: false, message: "please enter valid cart Id" });
+      let findCart = await cartModel.findOne({ _id: cartId, userId });
+      if (!findCart)
+        return res.status(404).send({ status: false, message: "cart not found" });
+  
+      if (!(removeProduct == 0 || removeProduct == 1))
+        return res
+          .status(400)
+          .send({
+            status: true,
+            message: "removeProduct value should be either 1 or 0",
+          });
+  
+      let itemsOfCart = findCart.items;
+      if (itemsOfCart.length == 0)
+        return res
+          .status(400)
+          .send({ status: false, message: "cart is already empty" });
+  
+      for (let i = 0; i < itemsOfCart.length; i++) {
+        if (itemsOfCart[i].productId == productId) {
+          let priceChange = itemsOfCart[i].quantity * findProduct.price;
+  
+          if (removeProduct == 0) {
+            const update = await cartModel.findOneAndUpdate(
+              { _id: cartId },
+              {
+                $pull: { items: { productId: productId } },
+                totalPrice: findCart.totalPrice - priceChange,
+                totalItems: findCart.totalItems - 1,
+              },
+              { new: true }
+            );
+            return res
+              .status(200)
+              .send({
+                status: true,
+                message: "Remove product Successfully",
+                data: update,
+              });
+          }
+  
+          if (removeProduct == 1) {
+            if (itemsOfCart[i].quantity == 1) {
+              const update = await cartModel.findOneAndUpdate(
+                { _id: cartId },
+                {
+                  $pull: { items: { productId: productId } },
+                  totalPrice: findCart.totalPrice - priceChange,
+                  totalItems: findCart.totalItems - 1,
+                },
+                { new: true }
+              );
+              return res
+                .status(200)
+                .send({
+                  status: true,
+                  message: "Remove product Successfully & price updated",
+                  data: update,
+                });
+            }
+  
+            itemsOfCart[i].quantity = itemsOfCart[i].quantity - 1;
+            const update = await cartModel.findOneAndUpdate(
+              { _id: cartId },
+              {
+                items: itemsOfCart,
+                totalPrice: findCart.totalPrice - findProduct.price,
+              },
+              { new: true }
+            );
+            return res
+              .status(200)
+              .send({
+                status: true,
+                message: "One item removed successfully",
+                data: update,
+              });
+          }
+        } else {
+          return res
+            .status(404)
+            .send({
+              status: false,
+              message: "please add the item in the cart first and then update",
+            });
+        }
+      }
+    } catch (err) {
+      return res.status(500).send({ status: false, message: err.message });
+    }
+  };
+  
+
+module.exports = { createCart, getCart, deleteCart,updateCart }
